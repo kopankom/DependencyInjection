0d79a3d31dfc09d532bb2e5e3861ce63092137a4
Michał Kopańko <mkopanko@pl.sii.eu>
2018-07-09 11:02:11 +0200

    F-10 refactor

--- DependencyInjection/DependencyInjectionContainer.py
+++ DependencyInjection/DependencyInjectionContainer.py
@@ -1,9 +1,9 @@
 class DependencyInjectionContainer():
     handlers = {}
 
-    def register_yaml_handler(self, yaml_handler, handler_name):
+    def register_yaml_handler(self, yaml_handler, container_name):
         yaml_handler.application_container = self
-        self.handlers[handler_name] = yaml_handler
+        self.handlers[container_name] = yaml_handler
 
     def add_file_content(self, content):
         for entry in content:
@@ -14,21 +14,18 @@ class DependencyInjectionContainer():
         return [handler for handler in self.handlers.values() if handler.yaml_entry_point == key][0]
 
     def get_handler_able_to_handle_value(self, value):
-        for handle in self.handlers:
-            if self.handlers[handle].can_i_handle_this(value):
-                return self.handlers[handle]
-        return None
+        for handler in self.handlers.values():
+            return handler if handler.can_i_handle_this(value) else None
 
     def bind_value(self, value):
         handler = self.get_handler_able_to_handle_value(value)
-        if None == handler:
+        if not handler:
             return value
         value = handler.bind_value(value)
         return self.bind_value(value)
 
     def compile_all_handlers(self):
-        for handle in self.handlers:
-            self.handlers[handle].compile()
+        [handler.compile() for handler in self.handlers.values()]
 
-    def get(self, key):
+    def get_container(self, key):
         return self.handlers[key]
