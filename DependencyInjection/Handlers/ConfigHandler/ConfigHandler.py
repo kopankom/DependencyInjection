2f9a06cd34f42cf356d94eaea167f14acdf28805
Michał Kopańko <mkopanko@pl.sii.eu>
2018-07-12 23:16:05 +0200

    F-10 refactor DependencyInjection class

--- DependencyInjection/Handlers/ConfigHandler/ConfigHandler.py
+++ DependencyInjection/Handlers/ConfigHandler/ConfigHandler.py
@@ -1,6 +1,5 @@
 from DependencyInjection.Handlers.HandlerAbstract import HandlerAbstract
 
-
 class ConfigHandler(HandlerAbstract):
     match_regex = '%([0-9a-zA-Z\.\_\-]+)%'
     yaml_entry_point = 'parameters'
